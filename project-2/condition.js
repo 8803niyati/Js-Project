804e4dbc86d220f50d457d2bdddf4ba2e1b283c9
Niyati Dhanani <niyatidhanani05@gmail.com>
2025-03-26 18:20:23 +0530

    condition statement

---
+++ project-2/condition.js
@@ -0,0 +1,108 @@
+// 1
+// let units=150;
+// let amount=0;
+// let surcharge=0;
+
+// if(units <= 50){
+//     amount + units *1;
+// }
+// else if(units <=150){
+//     amount= 50 * 1+ (units - 50) * 2;
+// }
+
+// else if(units <=250){
+//     amount =50 * 1 + 100 *2 +(units - 150) *3;
+// }
+// else{
+//     amount =50 * 1 + 100 *2 + 100 *2  +(units - 250) *4;
+//     surcharge + amount *0.2;
+//     // amount = amount +  surcharge;
+// }
+// console.log("total amount :",amount,"rupess");
+
+// ans=total amount : 250 rupess
+
+    
+2
+// let season = parseInt(prompt("enter uour number:"));
+// switch(season){
+//     case 1:
+//     case 2:
+//     case 3:
+//         console.log("winter");
+//         break;
+//         case 4:
+//         console.log("spring");
+//         break;
+//         case 5:
+//         case 6:
+//         case 7:
+//             console.log("summer");
+//         break;
+//         case 8:
+//             console.log("autem");
+//             break;
+//             default:
+//                 console.log("invalid season");
+//                 break;
+//            }
+//                 console.log(" season");
+
+//          ans :3 winter  
+
+// 3
+// let p= 100000;
+// let r;
+// let n =7;
+
+
+// if(3>n<=5){
+//      r=3;
+//      console.log(l=p*r*n/100);
+// }else{
+//       if(3>n<=8){
+//         r=3;
+//         console.log(l=p*r*n/100);
+//       }else{
+//         if(8<n<=12){
+//             console.log(l=p*r*n/100);
+//         }else{
+//             r=15;
+//             console.log(l=p*r*n/100);
+//         }
+//       }
+// }
+    
+// ans = 21000
+
+// 4
+
+// let age =  parseInt(prompt("enter uour age:"));
+// if(age>= 18) {
+//     console.log( " you are eligiable to vote.");
+// }else{
+//     console.log(" you are not  eligiable to vote.");
+// }
+
+// ans=12  you are not  eligiable to vote
+
+// 5
+
+
+// let age = 64;
+//    if(age < 0){
+//     console.log("invalid age");
+//   }
+//   else if(age <= 12){
+//     console.log("child");
+//   }
+//   else if(age <= 19){
+//     console.log("tenager");
+//   }
+//   else if(age <= 64){
+//     console.log("adluest");
+//   }
+//   else{
+//     console.log("senior citizen");
+//   }
+//  ans=adluest
