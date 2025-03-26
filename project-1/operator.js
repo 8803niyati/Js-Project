48790d65a2c1e06f28422215c507af907961afac
Niyati Dhanani <niyatidhanani05@gmail.com>
2025-03-26 10:30:25 +0530

    projec-1

---
+++ project-1/operator.js
@@ -0,0 +1,88 @@
+//1.  triangle
+// let a=10; b=7;
+// let area;
+// area = 1/2*(a*b);
+// console.log("area of triangle is :",area);
+
+// ans is:area of triangle is : 35
+
+// 2.
+
+// let celsius,fahrenheit;
+// celsius=1;
+// fahrenheit =(celsius * 9/5 ) + 32;
+// console.log("fahrenheit is:",fahrenheit);
+
+// ans==fahrenheit is: 33.8
+
+
+// fahrenheit = 33.9;
+// celsius = (fahrenheit - 32 )* 5/9;
+// console.log("celsius is:",celsius);
+
+// ans==celsius is: 1.0555555555555547
+
+// 3.rectangle
+
+// let a=10; b=7;
+// let area;
+// area = a*b;
+// console.log("area of rectangle is :",area);
+
+// ans==area of rectangle is : 70
+
+//4 cricle
+// let r=10;
+// let area;
+// area = 3.14*(r**2);
+// console.log("area of cricle is :",area );
+
+// ans==area of cricle is : 314
+
+// 5.feet into inches
+
+// let feet=5;
+// let inches = feet * 12;
+// console.log(feet + " feet is equal to " + inches   " inches");
+
+
+// 6.formula
+// 1.
+// let a=4,b=-8;
+// let result;
+// result =( a**2)-(b**2) === (a-b )*(a+b);
+// console.log(result);
+
+//  ans==true
+
+// 2.
+// let a=3,b=5;
+// let result;
+// result =( a=b)**2 === (a**2 ) -2 *(a*b) + (b**2);
+// console.log(result);
+
+// ans==false
+
+// 3.
+// let a=20,b=30,c=40;
+// let result;
+// result =( a+b+c)**2 === (a**2)+(b**2)+(c**2) +2 * (a*b) + 2*(a*c) + 2*(b* c);
+// console.log(result);
+
+// ans==true
+
+// 4.
+//  let a=20,b=30,c=40;
+// let result;
+//  result =( a+b+c)**2 === (a**2)+(b**2)+(c**2) +2 * (a*b) + 2*(a*c) + 2*(b* c);
+// console.log(result);
+
+// ans==true
+
+// 5.
+//  let a=20,b=30,c=40;
+// let result;
+//  result =(a-b)**3 ===(a**3) - 3 *(a **2) * b+3 * (a**2)* b-(b**3);
+// console.log(result);
+
+// ans==false
