292a57570f49c373b3a553795a1ae563167cfa71
Niyati Dhanani <niyatidhanani05@gmail.com>
2025-04-08 18:16:25 +0530

    function

---
+++ project-4/function.js
@@ -0,0 +1,96 @@
+// 1
+// function greet(message = "good morning"){
+//      console.log(message);
+// }
+// greet();
+
+// ans=good morning
+
+// / 2
+// function circlearea(redius){
+//     let area=3.14 * redius * redius;
+//     return area;
+// }
+//   console.log(circlearea(15));
+
+//   ans=706.5
+
+// 3
+// function  trianglearea(base,height){
+//     let area = (base * height) /2;
+//     return area;
+// }
+// console.log(trianglearea(10,20));
+
+// ans=100
+
+// 4
+// function  rectanglearea(length,height){
+//         let area = length * height;
+//         return area;
+//     }
+//     console.log(rectanglearea(50,50));
+
+//     ans=2500
+
+// 5
+// function ans(a,b,c){
+//     let ans=((b * b) * (4 * a* c))/( 2* a);
+//     return ans;
+// }
+//  console.log(ans(1,2,4));
+
+//  ans=32
+
+// 6
+// function ans(a,b){
+//         let ans=((a * a ) + (2 * a* b))+ ( b * b);
+//         return ans;
+//     }
+//      console.log(ans(2,3));
+
+// ans= 25  
+
+// 7
+// function fahrenheittocelsius(fahrenheit){
+//      let celsius= (fahrenheit - 32) / 1.8;
+//      return celsius;
+// }
+// console.log(fahrenheittocelsius(100));
+
+// nas=37.77777777777778
+
+// 8
+//  function celsiustofahrenheit(celsius){
+//          let fahrenheit = (celsius  * 1.8) + 32;
+//          return fahrenheit;
+//     }
+//     console.log( celsiustofahrenheit(0));
+
+//     ans=32
+
+// 9
+// function checkoddeven(num){
+//           if(num % 2 == 0){
+//             console.log("number is even" , num);
+//           } 
+//           else{
+//             console.log("number is odd" , num);
+//           }
+//      }
+//      checkoddeven(5);
+
+// ans=number is odd 5
+
+// 10
+// function swapvalues(a,b){
+//     a = a + b;
+//     b = a - b;
+//     a = a - b;
+//     console.log("value of a is :",a)
+//     console.log("value of b is :",b)
+// }
+// swapvalues(20,10);
+
+// ans=value of a is : 10
+// value of b is : 20                              
